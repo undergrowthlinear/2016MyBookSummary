b12a26d09c5d69cc02d5c0232ff86b1eaab79b6b
zhangwu <undergrowth@126.com>
2017-01-05 22:55:39 +0800

    初始化2016书单整理代码

---
+++ src/main/java/com/undergrowth/MybooksummaryApplication.java
@@ -0,0 +1,12 @@
+package com.undergrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybooksummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybooksummaryApplication.class, args);
+	}
+}
