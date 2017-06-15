ec542be8a51e405bbadebd0ded74f323363298c6
zhangwu <undergrowth@126.com>
2017-06-15 18:52:13 +0800

    添加GSON 添加REST

---
+++ src/main/java/com/undergrowth/google/gson/adapter/PointAdapter.java
@@ -0,0 +1,53 @@
+package com.undergrowth.google.gson.adapter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+
+/**
+ * 自定义适配器
+ *
+ * @author zhangwu
+ * @version 1.0.0
+ * @date 2017-05-12-16:09
+ */
+public class PointAdapter extends TypeAdapter<Point> {
+
+  public Point read(JsonReader reader) throws IOException {
+    if (reader.peek() == JsonToken.NULL) {
+      reader.nextNull();
+      return null;
+    }
+    String xy = reader.nextString();
+    String[] parts = xy.split(",");
+    int x = Integer.parseInt(parts[0]);
+    int y = Integer.parseInt(parts[1]);
+    return new Point(x, y);
+  }
+
+  public void write(JsonWriter writer, Point value) throws IOException {
+    if (value == null) {
+      writer.nullValue();
+      return;
+    }
+    String xy = value.getX() + "," + value.getY();
+    writer.value(xy);
+  }
+
+  public static void main(String[] arsg) {
+    GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(Point.class, new PointAdapter());
+    Gson gson = builder.create();
+    Point point = new Point(100, 200);
+    String jsonPoint = gson.toJson(point, Point.class);
+    System.out.println(jsonPoint);
+    Point otherPoint = gson.fromJson(jsonPoint, Point.class);
+    System.out.println(otherPoint);
+    System.out.println(otherPoint.equals(point));
+  }
+
+}
---
+++ src/main/java/com/undergrowth/google/gson/jsonadapter/UserInfo.java
@@ -0,0 +1,47 @@
+package com.undergrowth.google.gson.jsonadapter;
+
+import com.google.gson.annotations.JsonAdapter;
+
+/**
+ * 用户信息
+ *
+ * @author zhangwu
+ * @version 1.0.0
+ * @date 2017-05-12-17:24
+ */
+@JsonAdapter(UserJsonAdapter.class)
+public class UserInfo {
+
+  public String firstName;
+  public String lastName;
+
+  public UserInfo(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof UserInfo)) {
+      return false;
+    }
+
+    UserInfo userInfo = (UserInfo) o;
+
+    if (!firstName.equals(userInfo.firstName)) {
+      return false;
+    }
+    return lastName.equals(userInfo.lastName);
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstName.hashCode();
+    result = 31 * result + lastName.hashCode();
+    return result;
+  }
+}
