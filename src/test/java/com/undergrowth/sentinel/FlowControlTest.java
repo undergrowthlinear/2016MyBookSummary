17513c64a20e16a7bdba73b45b310c1b87308eec
zhangwu <undergrowth@126.com>
2018-11-27 14:10:56 +0800

    加入sentinel测试

---
+++ src/test/java/com/undergrowth/sentinel/FlowControlTest.java
@@ -0,0 +1,45 @@
+package com.undergrowth.sentinel;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhangwu
+ * @version 1.0.0
+ * @date 2018-11-27-10:50
+ */
+public class FlowControlTest {
+    public static void main(String[] args) {
+        initFlowRules();
+        while (true) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("HelloWorld");
+                System.out.println("hello world");
+            } catch (BlockException e1) {
+                System.out.println("block!");
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+        }
+    }
+
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<FlowRule>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}
